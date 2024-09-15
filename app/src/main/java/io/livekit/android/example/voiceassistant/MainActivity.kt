fcb3dbe825d8e75aa2d20c3d1bfe6781b2258c75
davidliu <davidliu@deviange.net>
2024-09-16 00:27:18 +0900

    rive visualization

--- app/src/main/java/io/livekit/android/example/voiceassistant/MainActivity.kt
+++ app/src/main/java/io/livekit/android/example/voiceassistant/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,13 +17,14 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import app.rive.runtime.kotlin.core.Loop
+import app.rive.runtime.kotlin.core.Rive
 import io.livekit.android.AudioOptions
 import io.livekit.android.LiveKit
 import io.livekit.android.LiveKitOverrides
@@ -34,6 +33,7 @@ import io.livekit.android.compose.local.RoomScope
 import io.livekit.android.example.voiceassistant.audio.LocalAudioTrackFlow
 import io.livekit.android.example.voiceassistant.state.AssistantState
 import io.livekit.android.example.voiceassistant.state.rememberAssistantState
+import io.livekit.android.example.voiceassistant.ui.ComposableRiveAnimationView
 import io.livekit.android.example.voiceassistant.ui.theme.LiveKitVoiceAssistantExampleTheme
 import io.livekit.android.room.Room
 import io.livekit.android.util.LoggingLevel
@@ -47,6 +47,7 @@ const val token =
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Rive.init(this)
         LiveKit.loggingLevel = LoggingLevel.DEBUG
         requireNeededPermissions {
             setContent {
@@ -81,12 +82,11 @@ fun VoiceAssistant(modifier: Modifier = Modifier) {
             connect = true,
             liveKitOverrides = overrides
         ) { room ->
-
             val (visualizer, stateIndicator) = createRefs()
 
-            Box(
+            Visualizer(
+                room = room,
                 modifier = Modifier
-                    .background(Color.Red)
                     .constrainAs(visualizer) {
                         height = Dimension.value(100.dp)
                         width = Dimension.value(100.dp)
@@ -95,9 +95,8 @@ fun VoiceAssistant(modifier: Modifier = Modifier) {
                         top.linkTo(parent.top)
                         bottom.linkTo(parent.bottom)
                     }
-            ) {
+            )
 
-            }
             StateIndicator(
                 room = room,
                 modifier = Modifier.constrainAs(stateIndicator) {
@@ -156,4 +155,54 @@ fun KeepScreenOn() {
             currentView.keepScreenOn = false
         }
     }
+}
+
+@Composable
+fun Visualizer(room: Room, modifier: Modifier) {
+    val roomState by room::state.flow.collectAsState(initial = Room.State.CONNECTING)
+
+    val remoteParticipants by room::remoteParticipants.flow.collectAsState()
+    val remoteParticipant by remember(remoteParticipants) {
+        derivedStateOf { remoteParticipants.values.firstOrNull() }
+    }
+    val assistantState = rememberAssistantState(participant = remoteParticipant)
+
+
+    ComposableRiveAnimationView(
+        modifier = modifier,
+        animation = R.raw.avatar,
+        update = {
+            val animationName = when (roomState) {
+                Room.State.CONNECTING -> "open"
+                Room.State.RECONNECTING -> "waiting"
+                Room.State.DISCONNECTED -> null
+                Room.State.CONNECTED -> {
+                    when (assistantState) {
+                        AssistantState.LISTENING -> "waiting"
+                        AssistantState.THINKING -> "thinking"
+                        AssistantState.SPEAKING -> "writing"
+                        AssistantState.UNKNOWN -> null
+                    }
+                }
+            }
+
+            it.stop()
+
+            if (animationName != null) {
+                val loop = when (animationName) {
+                    "waiting",
+                    "thinking",
+                    "writing" -> {
+                        Loop.LOOP
+                    }
+
+                    else -> {
+                        Loop.ONESHOT
+                    }
+                }
+
+                it.play(animationName, loop)
+            }
+        }
+    )
 }
